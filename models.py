e87e05c33e2c846ca11062a579c724c3d8a86ab0
gunt2raro <gunt_raro@hotmail.com>
2015-10-09 06:53:56 +0000

    models ha

--- models.py
+++ models.py
@@ -1,13 +1,6 @@
 # Libs
 from django.db import models
 from django.contrib.auth.models import User
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
-
-#Shit I don't know
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 """
 Icon model
@@ -77,27 +70,6 @@ class Media( models.Model ) :
     #End of unicode function
     
 #End of Media model
-
-"""
-Snippets model
-"""
-class Snippet( models.Model ) :
-        
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    code = models.TextField()
-    linenos = models.BooleanField(default=False)
-    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
-        
-    """
-    Meta class
-    """
-    class Meta :
-        ordering = ( 'created', )
-    #End of meta class
-    
-#End of snippets model
 
 """
 Comment 
